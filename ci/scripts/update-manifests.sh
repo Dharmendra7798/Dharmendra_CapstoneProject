521db300f7ade72edb8496b4c16c3e98bfb022e9
Dharmendra <42884058+Dharmendra7798@users.noreply.github.com>
2025-10-20 01:36:15 +0530

    Update update-manifests.sh

--- ci/scripts/update-manifests.sh
+++ ci/scripts/update-manifests.sh
@@ -7,17 +7,17 @@ GIT_USER=$3
 GIT_PASS=$4
 
 # Update frontend deployment
-sed -i "s|image: .*frontend.*|image: $FRONTEND_IMAGE:latest|" ./k8s/apps/frontend/frontend-deployment.yaml
+sed -i "s|image: .*frontend.*|image: $FRONTEND_IMAGE|" ./k8s/apps/frontend/frontend-deployment.yaml
 
 # Update backend deployment
-sed -i "s|image: .*backend.*|image: $BACKEND_IMAGE:latest|" ./k8s/apps/backend/backend-deployment.yaml
+sed -i "s|image: .*backend.*|image: $BACKEND_IMAGE|" ./k8s/apps/backend/backend-deployment.yaml
 
 # Commit and push
 git config user.email "jenkins@example.com"
 git config user.name "Jenkins CI"
 
 # Stage all YAMLs recursively
-git add .
+git add ./k8s/apps
 
 # Commit only if there are changes
 git diff --cached --quiet || git commit -m "Update frontend & backend images to latest"
