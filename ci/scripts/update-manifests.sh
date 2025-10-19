0071759194d06963fa1ca023434b072aaa36f66f
getdevopsready <getdevopsready@gmail.com>
2025-10-20 01:17:47 +0530

    file upd

--- ci/scripts/update-manifests.sh
+++ ci/scripts/update-manifests.sh
@@ -5,14 +5,14 @@ FRONTEND_IMAGE=$1
 BACKEND_IMAGE=$2
 
 # Update frontend deployment
-sed -i "s|image: .*frontend.*|image: $FRONTEND_IMAGE:latest|" ./k8s/apps/frontend-deployment.yaml
+sed -i "s|image: .*frontend.*|image: $FRONTEND_IMAGE:latest|" ./k8s/apps/frontend/frontend-deployment.yaml
 
 # Update backend deployment
-sed -i "s|image: .*backend.*|image: $BACKEND_IMAGE:latest|" ./k8s/apps/backend-deployment.yaml
+sed -i "s|image: .*backend.*|image: $BACKEND_IMAGE:latest|" ./k8s/apps/backend/backend-deployment.yaml
 
 # Commit and push
 git config user.email "jenkins@example.com"
 git config user.name "Jenkins CI"
-git add ./k8s/apps/*.yaml
+git add ./k8s/apps/**/*.yaml
 git commit -m "Update frontend & backend images to latest"
 git push origin main
